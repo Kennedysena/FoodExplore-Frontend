f2167b0cc5f341aae1974f1f5e9e93fc956174eb
Kennedy Sena <kennedysena.dev@gmail.com>
2025-01-04 16:05:28 -0400

    style: Adjustment to form title and edge rounding.

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -24,7 +24,7 @@ export const Form = styled.form`
   height: 54.0rem;
   width: 47.6rem;
   padding: 6.4rem;
-  border-radius: .8rem;
+  border-radius: 1.6rem;
   background-color: ${({ theme }) => theme.COLORS.DARK_600};
 
   display: flex;
@@ -34,7 +34,7 @@ export const Form = styled.form`
 
   > h1 {
     font-size: 3.2rem;
-    font-weight: 500;
+    font-weight: 400;
     color: ${({ theme }) => theme.COLORS.LIGHT_100};
     font-family: "Poppins", sans-serif;
     text-align: center;
