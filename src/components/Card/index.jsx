2e2a7179df5c5293d18580bc2a35513aa74fcee6
Kennedy Sena <kennedysena.dev@gmail.com>
2025-01-06 19:35:34 -0400

    style: Adjusted the style of the Card component and added the Brand component to the Home page. The route to the Dish page was also created.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,18 +1,17 @@
-import { Container, Title, Order, Button } from "./styles";
+import { Container, Brand, Title, Order, Button } from "./styles";
 import { FiHeart } from "react-icons/fi";
 import { RxCaretRight } from "react-icons/rx";
 import { Number } from "../Number";
-import { Link } from "react-router-dom";
 export function Card({ img, title, description, price }) {
   return (
     <Container>
       <FiHeart />
-      <img src={img} alt="Imagem do prato" />
-      <Title>
+      <Brand to="/dish">
+        <img src={img} alt="Imagem do prato" />
+      </Brand>
+      <Title to="/dish">
         <h2>{title}</h2>
-        <Link to="/dish">
-          <RxCaretRight size={24}/>
-        </Link>
+        <RxCaretRight size={35} />
       </Title>
       <p>{description}</p>
       <span>{price}</span>
